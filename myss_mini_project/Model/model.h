4ccf0abb014536def44160d81a20c4d2cf548130
phoenixsac <42166461+phoenixsac@users.noreply.github.com>
2024-08-30 05:11:44 +0530

    Update model.h

--- myss_mini_project/Model/model.h
+++ myss_mini_project/Model/model.h
@@ -1,6 +1,9 @@
 #ifndef MODEL_H
 #define MODEL_H
 
+// This file includes other header files: student.h, faculty.h, course.h, and user_type.h. By including these headers, it allows the current file (model.h)
+// to use the declarations and definitions from those headers without having to include each of them individually in every file that requires them.
+
 #include "student.h"
 #include "faculty.h"
 #include "course.h"
