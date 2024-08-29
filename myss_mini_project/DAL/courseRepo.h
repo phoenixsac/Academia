3519109e3671d448af340229667362c98147bd61
phoenixsac <42166461+phoenixsac@users.noreply.github.com>
2024-08-30 04:50:49 +0530

    Update courseRepo.h

--- myss_mini_project/DAL/courseRepo.h
+++ myss_mini_project/DAL/courseRepo.h
@@ -7,10 +7,16 @@
 #include "../Model/model.h"
 #include "../config.h"
 
+// Function to get the next course ID by reading the last course record in the file
 int getNextCourseId()
 {
+    // Variables to store the number of bytes read/written
+    // Variable to store the last course record
     ssize_t readBytes, writeBytes;
     Course previousCourse;
+
+    // Open the course file in read-only mode
+    // If the file doesn't exist, return 1 as the first course ID (handle file opening errors)
     int CourseFileDescriptor = open(COURSE_FILE, O_RDONLY);
     if (CourseFileDescriptor == -1 && errno == ENOENT)
     {
@@ -24,6 +30,7 @@ int getNextCourseId()
     }
     else
     {
+        // Seek to the last course record in the file
         int offset = lseek(CourseFileDescriptor, -sizeof(Course), SEEK_END);
         if (offset == -1)
         {
@@ -31,6 +38,7 @@ int getNextCourseId()
             return false;
         }
 
+        // Setup a read lock on the last course record
         struct flock lock = {F_RDLCK, SEEK_SET, offset, sizeof(Course), getpid()};
         int lockingStatus = fcntl(CourseFileDescriptor, F_SETLKW, &lock);
         if (lockingStatus == -1)
@@ -39,6 +47,7 @@ int getNextCourseId()
             return false;
         }
 
+        // Read the last course record
         readBytes = read(CourseFileDescriptor, &previousCourse, sizeof(Course));
         if (readBytes == -1)
         {
@@ -46,16 +55,20 @@ int getNextCourseId()
             return false;
         }
 
+        // Unlock the record after reading
         lock.l_type = F_UNLCK;
         fcntl(CourseFileDescriptor, F_SETLK, &lock);
 
+        // close file
         close(CourseFileDescriptor);
 
+        // return next course id
         return previousCourse.id + 1;
     }
 
 }
 
+// create a course and append it to the end of a course file
 void createCourse(Course newCourse)
 {
     ssize_t readBytes, writeBytes;
@@ -65,18 +78,24 @@ void createCourse(Course newCourse)
         perror("Error while creating / opening Course file!");
         return;
     }
+
+     // Setup a write lock on the file
     struct flock lock;
     lock.l_type = F_WRLCK;
     lock.l_whence = SEEK_END;
     lock.l_start = 0;
-    lock.l_len = sizeof(Student);
+    lock.l_len = sizeof(Course);
     lock.l_pid = getpid();
+
+    // handle if any locking errors
     int lockingStatus = fcntl(CourseFileDescriptor, F_SETLKW, &lock);
     if (lockingStatus == -1)
     {
         perror("Error while obtaining write lock on course record!");
         return;
     }
+
+    // Write the new course record to the file
     writeBytes = write(CourseFileDescriptor, &newCourse, sizeof(newCourse));
     if (writeBytes == -1)
     {
@@ -84,12 +103,14 @@ void createCourse(Course newCourse)
         return;
     }
 
+    // unlock file
     lock.l_type = F_UNLCK;
     fcntl(CourseFileDescriptor, F_SETLKW, &lock);
-
+    // close file
     close(CourseFileDescriptor);
 }
 
+// Function to retrieve a course record by its ID
 Course getCourseById(int courseID)
 {
     Course course;
@@ -98,6 +119,8 @@ Course getCourseById(int courseID)
     off_t offset;
     int lockingStatus;
     int courseFileDescriptor = open(COURSE_FILE, O_RDONLY);
+
+    // seek to just the top row of the required course by id * sizeOf course
     offset = lseek(courseFileDescriptor, (courseID-1) * sizeof(Course), SEEK_SET);
     if (offset == -1)
     {
@@ -125,33 +148,42 @@ Course getCourseById(int courseID)
     lock.l_type = F_UNLCK;
     fcntl(courseFileDescriptor, F_SETLK, &lock);
 
+    // close file and return course
     close(courseFileDescriptor);
 
     return course;
 
 }
 
+// Function to update an existing course record in the file
 bool updateCourse(Course course)
 {
     ssize_t writeBytes;
+
+    // Open the course file in write-only mode
     int courseFileDescriptor = open(COURSE_FILE, O_WRONLY);
     if (courseFileDescriptor == -1)
     {
         perror("Error while opening course file");
         return false;
     }
+    // move to the start of the required course by using seek_set
     int offset = lseek(courseFileDescriptor, (course.id-1) * sizeof(Course), SEEK_SET);
     if (offset == -1)
     {
         perror("Error while seeking to required course record!");
         return false;
     }
+
+    // write lock implementation
     struct flock lock;
     lock.l_type = F_WRLCK;
     lock.l_start = offset;
     lock.l_whence = SEEK_SET;
     lock.l_len = sizeof(Course);
     lock.l_pid = getpid();
+
+    // manage locking status error handling
     int lockingStatus = fcntl(courseFileDescriptor, F_SETLKW, &lock);
     if (lockingStatus == -1)
     {
@@ -159,12 +191,14 @@ bool updateCourse(Course course)
         return false;
     }
 
+    // write the passed on Course, (overwrite) over the existing one
     writeBytes = write(courseFileDescriptor, &course, sizeof(Course));
     if (writeBytes == -1)
     {
         perror("Error while writing update course into file");
     }
 
+    // unlock lock, close file and return true to indicate successful modification
     lock.l_type = F_UNLCK;
     fcntl(courseFileDescriptor, F_SETLK, &lock);
 
